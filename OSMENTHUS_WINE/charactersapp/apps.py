6ca9b904297abdca1a050da97d5ffbad9db821b4
TF5S <91826877+0805200@users.noreply.github.com>
2022-03-27 16:00:26 +0600

    уъу

--- OSMENTHUS_WINE/charactersapp/apps.py
+++ OSMENTHUS_WINE/charactersapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CharctersappConfig(AppConfig):
+class CharactersappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'charctersapp'
+    name = 'charactersapp'
---
+++ OSMENTHUS_WINE/charactersapp/models.py
@@ -0,0 +1,19 @@
+from email.base64mime import header_length
+from unicodedata import name
+from xml.dom.domreg import well_known_implementations
+from xml.dom.minidom import Element
+from django.db import models
+
+class GameCharacter(models.Model):
+    characterName = models.CharField(max_length = 10)
+    damage = models.IntegerField()
+    health = models.IntegerField()
+    high = models.IntegerField()
+    weapon = models.CharField(max_length = 10)
+    element = models.CharField(max_length = 10)
+    birthday = models.IntegerField()
+    c = models.CharField(max_length = 20)
+    avatar = models.ImageField(blank = True)
+
+
+
